ed6f47a0a6d299936b4a2a908f83b1f630a1d2be
LexusPC <dlexusb@gmail.com>
2022-02-22 00:22:10 +0300

    feat third and fourth patters/added new font for first and second pats

--- patterns/fourth_pattern.py
+++ patterns/fourth_pattern.py
@@ -5,14 +5,14 @@ class FourthPattern():
     template = 'fourth_temp.html'
 
     def __init__(self, data):
-        # data = re.split('\r\n', data)
 
         data = [re.split(':', itm) for itm in re.split('\r\n', data)]
         if data[-1][0] == '':
             data.remove([''])
 
         self.data = data
+        self.cost_count = len(data)
 
     def render_temp(self):
 
-        return render_template(self.template, data = self.data)
+        return render_template(self.template, data = self.data, cost_count = self.cost_count)
